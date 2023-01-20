3688496cd3c0a8a752dbcc038ffcf1e3f7f3b9c4
AdrienB4000 <belferadrien@gmail.com>
2023-01-20 14:00:16 +0100

    don't take into account impossible events

--- markovSim.jl
+++ markovSim.jl
@@ -8,12 +8,14 @@ function simulateMarkov(maxTime::Int)::Vector{Float64}
         - part of the total time where each station was empty when a customer arrived.
     """
 
-    data = readData("data_50uniform.txt")
+    data = readData("data_1.txt")
     state = initState(data)
-    events = vcat([i for i in 1:data.nbSt], [(i, j) for i in 1:data.nbSt for j in 1:data.nbSt if i!=j])
-    lambdas = vcat(data.lambdas, [1/data.travelTime[i, j] for i in 1:data.nbSt for j in 1:data.nbSt if i!=j])
+    events = vcat([i for i in 1:data.nbSt], [(i, j) for i in 1:data.nbSt for j in 1:data.nbSt])
+    lambdas = vcat(data.lambdas, [i == j ? 0 : 1/data.travelTime[i, j] for i in 1:data.nbSt for j in 1:data.nbSt])
     lambdasSum = sum(lambdas)
-    eventsFrequency = [lamb/lambdasSum for lamb in lambdas]
+    lambdasFeasible = vcat([state.parkedBicycles[i] == 0 ? 0 : data.lambdas[i] for i in 1:data.nbSt],
+                           [state.transitBicycles[i, j] == 0 ? 0 : 1/data.travelTime[i, j] for i in 1:data.nbSt for j in 1:data.nbSt])
+    lambdasFeasibleSum = sum(lambdasFeasible)
     currentTime = 0
     nextTime = min(exponentialLaw(lambdasSum), maxTime)
     stationEmptinessTime = [0. for _ in 1:data.nbSt]
@@ -27,23 +29,37 @@ function simulateMarkov(maxTime::Int)::Vector{Float64}
                 stationEmptinessTime[st] += nextTime - currentTime
             end
         end
-        event = sample(events, Weights(eventsFrequency))
+        event = sample(events, Weights(lambdasFeasible))
         if typeof(event) == Int64
             # Here the event is that a customer arrives at a station
             if state.parkedBicycles[event] > 0
                 j = sample(data.allSt, Weights(data.routage[event, :]))
+                if state.transitBicycles[event, j] == 0
+                    lambdasFeasible[data.nbSt + (event - 1) * data.nbSt + j] += lambdas[data.nbSt + (event - 1) * data.nbSt + j]
+                    lambdasFeasibleSum += lambdas[data.nbSt + (event - 1) * data.nbSt + j]
+                end
                 takeOneFromIToJ(state, event, j)
+                if state.parkedBicycles[event] == 0
+                    lambdasFeasible[event] = 0
+                    lambdasFeasibleSum -= lambdas[event]
+                end
             end
         elseif typeof(event) == Tuple{Int64, Int64}
             # Here the event is that a customer finishes is travel from i to j
-            if state.transitBicycles[event[1], event[2]] > 0
+            if state.transitBicycles[event[1], event[2]] > 0 #useless if lambdasFeasible is OK
+                if state.parkedBicycles[event[2]] == 0
+                    lambdasFeasible[event[2]] = lambdas[event[2]]
+                    lambdasFeasibleSum += lambdas[event[2]]
+                end
                 arriveFromIAtJ(state, event[1], event[2])
+                lambdasFeasible[data.nbSt + (event[1] - 1) * data.nbSt + event[2]] -= lambdas[data.nbSt + (event[1] - 1) * data.nbSt + event[2]]
+                lambdasFeasibleSum -= lambdas[data.nbSt + (event[1] - 1) * data.nbSt + event[2]]
             end
         else
             println("Big problem here")
         end
         currentTime = nextTime
-        nextTime = min(currentTime + exponentialLaw(lambdasSum), maxTime)
+        nextTime = min(currentTime + exponentialLaw(lambdasFeasibleSum), maxTime)
     end
     # We finish the simulation between currentTime and maxTime
     for st in 1:data.nbSt
